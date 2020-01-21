5b10c93875eb667067b3e018b7115122c1f53ede
Mark Llobrera <mark@dirtystylus.com>
2020-01-21 16:45:06 -0500

    large media

--- src/utils/figure.js
+++ src/utils/figure.js
@@ -2,6 +2,12 @@
 module.exports = (image, caption, className) => {
   const classMarkup = className ? ` class="${className}"` : '';
   const captionMarkup = caption ? `<figcaption>${caption}</figcaption>` : '';
-  return `<figure${classMarkup}><img src="/img/${image}" />${captionMarkup}</figure>`;
+  return `<figure${classMarkup}>
+  <picture>
+  <source srcset="/img/${image}?nf_resize=fit&w=700" media="(min-width: 1200px)">
+  <source srcset="/img/${image}?nf_resize=fit&w=600" media="(min-width: 740px)">
+  <img src="/img/${image}" /></picture>
+  ${captionMarkup}
+  </figure>`;
   //return `<figure${className ? ` class="${className}"` : ''}><img src="/img/${image}" />${caption ? `<figcaption>${caption}</figcaption>` : ''}</figure>`;
 };
