8b244b1df8b2a80f31c8059ad8edb8dcaee33817
Mark Llobrera <mark@dirtystylus.com>
2020-02-12 16:30:05 -0500

    added logic for srcset based on image style name

--- src/utils/figure.js
+++ src/utils/figure.js
@@ -1,12 +1,30 @@
 // Figure Shortcode
-module.exports = (image, caption, className) => {
-  const classMarkup = className ? ` class="${className}"` : '';
+module.exports = (image, caption, styleName) => {
+  const styleObj = {
+    cinemascope: [
+      { width: 700, breakwidth: 650 },
+      { width: 1200, breakwidth: 1000 }
+    ],
+    book_thumb: [
+      { width: 300, breakwidth: 400 }
+    ],
+    default: [
+      { width: 700, breakwidth: 650 }
+    ]
+  };
+
+  const styleItem = styleObj[styleName] ? styleObj[styleName] : styleObj['default'];
+  const classMarkup = styleName ? ` class="${styleName}"` : '';
   const captionMarkup = caption ? `<figcaption>${caption}</figcaption>` : '';
-  const srcsetMarkup = className === 'cinemascope' ? `<source srcset="/img/${image}?nf_resize=fit&w=1200" media="(min-width: 1000px)"><source srcset = "/img/${image}?nf_resize=fit&w=700" media = "(min-width: 650px)" >` : `<source srcset = "/img/${image}?nf_resize=fit&w=700" media = "(min-width: 650px)" >`;
+  // const srcsetMarkup = className === 'cinemascope' ? `<source srcset="/img/${image}?nf_resize=fit&w=1200" media="(min-width: 1000px)"><source srcset = "/img/${image}?nf_resize=fit&w=700" media = "(min-width: 650px)" >` : `<source srcset = "/img/${image}?nf_resize=fit&w=700" media = "(min-width: 650px)" >`;
+  let srcsetMarkup = '';
+  styleItem.forEach(element => {
+    srcsetMarkup += `<source srcset="/img/${image}?nf_resize=fit&w=${element.width}" media="(min-width: ${element.breakwidth}px)"></source>`;
+  });
   return `<figure${classMarkup}>
   <picture>
   ${srcsetMarkup}
-  <img src="/img/${image}?nf_resize=fit&w=500" /></picture>
+  <img src="/img/${image}?nf_resize=fit&w=400" /></picture>
   ${captionMarkup}
   </figure>`;
   //return `<figure${className ? ` class="${className}"` : ''}><img src="/img/${image}" />${caption ? `<figcaption>${caption}</figcaption>` : ''}</figure>`;
