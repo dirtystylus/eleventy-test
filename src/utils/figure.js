d9828c48a01caf03f68dbde4fd13b2db8cc3418f
Mark Llobrera <mark@dirtystylus.com>
2020-06-12 14:33:01 -0400

    setting new image width for non-cinemascope images in widest grid set

--- src/utils/figure.js
+++ src/utils/figure.js
@@ -3,10 +3,10 @@ module.exports = (image, caption, styleName) => {
   const styleObj = {
     cinemascope: [
       { width: 1200, breakwidth: 1000 },
-      { width: 700, breakwidth: 650 }
+      { width: 850, breakwidth: 650 },
     ],
     book_thumb: [{ width: 300, breakwidth: 400 }],
-    default: [{ width: 700, breakwidth: 650 }]
+    default: [{ width: 700, breakwidth: 650 }],
   };
 
   const styleItem = styleObj[styleName]
@@ -16,7 +16,7 @@ module.exports = (image, caption, styleName) => {
   const captionMarkup = caption ? `<figcaption>${caption}</figcaption>` : "";
   // const srcsetMarkup = className === 'cinemascope' ? `<source srcset="/img/${image}?nf_resize=fit&w=1200" media="(min-width: 1000px)"><source srcset = "/img/${image}?nf_resize=fit&w=700" media = "(min-width: 650px)" >` : `<source srcset = "/img/${image}?nf_resize=fit&w=700" media = "(min-width: 650px)" >`;
   let srcsetMarkup = "";
-  styleItem.forEach(element => {
+  styleItem.forEach((element) => {
     srcsetMarkup += `<source srcset="/img/${image}?nf_resize=fit&w=${element.width}" media="(min-width: ${element.breakwidth}px)"></source>`;
   });
   return `<figure${classMarkup}>
