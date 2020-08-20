e8577684382c4cf29a2661864b0d673b93b64f5f
Mark Llobrera <mark@dirtystylus.com>
2020-08-20 14:02:01 -0400

    added page-level pagination for books list

--- .eleventy.js
+++ .eleventy.js
@@ -91,17 +91,17 @@ module.exports = function (eleventyConfig) {
   // Next/Previous navigation
   // See: https://brycewray.com/posts/2019/12/previous-next-eleventy/
   eleventyConfig.addCollection("posts", function (collection) {
-    const coll = collection.getFilteredByTag("posts").reverse();
+    const coll = collection.getFilteredByTag("posts");
 
     for (let i = 0; i < coll.length; i++) {
-      const prevPost = coll[i + 1];
-      const nextPost = coll[i - 1];
+      const prevPost = coll[i - 1];
+      const nextPost = coll[i + 1];
 
       coll[i].data["prevPost"] = prevPost;
       coll[i].data["nextPost"] = nextPost;
     }
 
-    return coll;
+    return [...coll].reverse();
   });
 
   /* Markdown Overrides */
