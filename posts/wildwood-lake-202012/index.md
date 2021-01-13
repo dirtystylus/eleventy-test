510102c3d542958eb12187198ee60e9b01ed73a9
Mark Llobrera <mark@dirtystylus.com>
2021-01-13 10:35:56 -0500

    Wildwood Lake gallery

---
+++ posts/wildwood-lake-202012/index.md
@@ -0,0 +1,30 @@
+---
+title: Wildwood Lake, December 2020
+display_title: Wildwood Lake, December 2020
+description: A post-Christmas walk in Harrisburg, PA.
+date: 2021-01-13T14:00:00-05:00
+layout: layouts/post-gallery
+tags:
+  - photos
+---
+
+The holidays were quiet; due to Covid-19 we elected not to visit family. Instead we celebrated at home, and scheduled an outdoor meeting at [Wildwood Lake](https://www.wildwoodlake.org) in Harrisburg, PA—which is roughly at the halfway point between our home and Jordan’s parents’. We took a walk around the lake, exchanged gifts, and drove back home. 
+
+The portion of the [Pennsylvania Canal](https://en.wikipedia.org/wiki/Pennsylvania_Canal) along the west side of the lake had partly frozen over, leading to some interesting patterns in the ice. 
+
+Taken with a Fujifilm X-T30 + 35mm/f2, Acros-R film sim.[^1]
+
+{% gallery %}
+
+- ![Tree emerging from cracked ice](wildwood-lake-1.jpg "Frozen lightning"){data-responsiver=gallery_2x3}
+- ![Heart-shaped frozen ripple](wildwood-lake-2.jpg "I heart you"){data-responsiver=gallery_3x2}
+- ![Broken stump](wildwood-lake-3.jpg "This one looks like a ruined tower"){data-responsiver=gallery_3x2}
+- ![Swirl frozen ripple.](wildwood-lake-4.jpg "Swirl"){data-responsiver=gallery_3x2}
+- ![Perfect frozen circle around tree base](wildwood-lake-5.jpg "Clean circle"){data-responsiver=gallery_2x3}
+- ![Small stump with a smooth frozen circle](wildwood-lake-6.jpg "Lonely stump"){data-responsiver=gallery_3x2}
+- ![Frozen ripples at water’s edge](wildwood-lake-7.jpg "Topographical map ice"){data-responsiver=gallery_3x2}
+- ![Susquehanna Spillway, with dangling icicles](wildwood-lake-8.jpg "Susquehanna Spillway"){data-responsiver=gallery_3x2}
+
+{% endgallery %}
+
+[^1]: I’m trying out a new gallery system which uses [PhotoSwipe](https://photoswipe.com). If you notice any issues, please let me know.
--- src/utils/images-responsiver-config.js
+++ src/utils/images-responsiver-config.js
@@ -3,144 +3,147 @@ const md = new markdownIt();
 const dbg = require("debug")("responsiver-config");
 
 const runAfterHook = (image, document) => {
-  let imageUrl =
-    image.getAttribute("data-pristine") || image.getAttribute("src");
-  let caption = image.getAttribute("title");
-  if (caption !== null) {
-    caption = md.renderInline(caption.trim());
-  }
+	let imageUrl =
+		image.getAttribute("data-pristine") || image.getAttribute("src");
+	let caption = image.getAttribute("title");
+	if (caption !== null) {
+		caption = md.renderInline(caption.trim());
+	}
 
-  let zoom = [...image.classList].indexOf("zoom") !== -1;
+	let zoom = [...image.classList].indexOf("zoom") !== -1;
 
-  if (caption || zoom) {
-    const figure = document.createElement("figure");
-    figure.classList.add(...image.classList);
-    // TODO: decide whether classes should be removed from the image or not
-    image.classList.remove(...image.classList);
-    let figCaption = document.createElement("figcaption");
-    figCaption.innerHTML =
-      (caption ? caption : "") +
-      (zoom
-        ? `<p class="zoom">&#128269; See <a href="${imageUrl}">full size</a></p>`
-        : "");
-    figure.appendChild(image.cloneNode(true));
-    figure.appendChild(figCaption);
+	if (caption || zoom) {
+		const figure = document.createElement("figure");
+		figure.classList.add(...image.classList);
+		// TODO: decide whether classes should be removed from the image or not
+		image.classList.remove(...image.classList);
+		let figCaption = document.createElement("figcaption");
+		figCaption.innerHTML =
+			(caption ? caption : "") +
+			(zoom
+				? `<p class="zoom">&#128269; See <a href="${imageUrl}">full size</a></p>`
+				: "");
+		figure.appendChild(image.cloneNode(true));
+		figure.appendChild(figCaption);
 
-    // Parent node of the image is a <p> because image is an inline element,
-    // and Markdown will wrap in a < p > tag
-    if (image.parentNode.nodeName === "p") {
-      image.parentNode.replaceWith(figure);
-    }
-  }
+		// Parent node of the image is a <p> because image is an inline element,
+		// and Markdown will wrap in a < p > tag
+		if (image.parentNode.nodeName === "p") {
+			image.parentNode.replaceWith(figure);
+		}
+	}
 };
 
 const runAfterHookGallery = (image, document) => {
-  let imageUrl =
-    image.getAttribute("data-pristine") || image.getAttribute("src");
-    let caption = image.getAttribute("title");
-    if (caption !== null) {
-      caption = md.renderInline(caption.trim());
-    }
-    
-  if (caption) {
-    const link = document.createElement("a");
-    link.setAttribute("href", imageUrl);
-    link.setAttribute("data-size", image.classList[0]);
-    const figure = document.createElement("figure");
-    figure.classList.add(...image.classList);
-    // TODO: decide whether classes should be removed from the image or not
-    image.classList.remove(...image.classList);
-    let figCaption = document.createElement("figcaption");
-    figCaption.innerHTML =
-      (caption ? caption : "");
-    link.appendChild(image.cloneNode(true));
-    figure.appendChild(link);
-    figure.appendChild(figCaption);
+	let imageUrl =
+		image.getAttribute("data-pristine") || image.getAttribute("src");
+		let caption = image.getAttribute("title");
+		if (caption !== null) {
+			caption = md.renderInline(caption.trim());
+		}
+		
+	const figure = document.createElement("figure");
+		figure.classList.add(...image.classList);
+		// TODO: decide whether classes should be removed from the image or not
+		image.classList.remove(...image.classList);
+		
+		const link = document.createElement("a");
+	link.setAttribute("href", imageUrl);
+		link.setAttribute("data-size", figure.classList[0]);
+		link.appendChild(image.cloneNode(true));
+		
+		figure.appendChild(link);
+		if (caption) {
+		let figCaption = document.createElement("figcaption");
+		figCaption.innerHTML =
+			(caption ? caption : "");
+		figure.appendChild(figCaption);
+			
+		}
 
-    // Parent node of the image is a <p> because image is an inline element,
-    // and Markdown will wrap in a < p > tag
-    if (image.parentNode.nodeName === "p") {
-      image.parentNode.replaceWith(figure);
-    } else {
-      image.replaceWith(figure);
-    }
-  }
+		// Parent node of the image is a <p> because image is an inline element,
+		// and Markdown will wrap in a < p > tag
+		if (image.parentNode.nodeName === "p") {
+			image.parentNode.replaceWith(figure);
+		} else {
+			image.replaceWith(figure);
+		}
 };
 
 module.exports = {
-  default: {
-    sizes: "(min-width: 65em) 1040px, (min-width: 45em) 800px, 100vw",
-    attributes: {
-      loading: "lazy",
-    },
-    resizedImageUrl: (src, width) => `${src}?nf_resize=fit&w=${width}`,
-    runAfter: runAfterHook,
-  },
-  regular: {
-    classes: ["regular"],
-  },
-  cinemascope: {
-    sizes: "(min-width: 65em) 1280px, (min-width: 45em) 800px, 100vw",
-    classes: ["cinemascope"],
-  },
-  book_thumb: {
-    sizes: "400px, 100vw",
-    classes: ["book-thumb"],
-    figure: "never",
-  },
-  gallery_3x2: {
-    sizes: "(min-width: 45em) 400px, 100vw",
-    classes: ["gallery-3x2"],
-    fallbackWidth: 400,
-    minWidth: 320,
-    maxWidth: 640,
-    steps: 3,
-    runAfter: runAfterHookGallery,
-  },
-  gallery_2x3: {
-    sizes: "(min-width: 45em) 400px, 100vw",
-    classes: ["gallery-2x3"],
-    fallbackWidth: 400,
-    minWidth: 320,
-    maxWidth: 640,
-    steps: 3,
-    runAfter: runAfterHookGallery,
-  },
-  gallery_4x3: {
-    sizes: "(min-width: 45em) 400px, 100vw",
-    classes: ["gallery-4x3"],
-    fallbackWidth: 400,
-    minWidth: 320,
-    maxWidth: 640,
-    steps: 3,
-    runAfter: runAfterHookGallery,
-  },
-  gallery_3x4: {
-    sizes: "(min-width: 45em) 400px, 100vw",
-    classes: ["gallery-3x4"],
-    fallbackWidth: 400,
-    minWidth: 320,
-    maxWidth: 640,
-    steps: 3,
-    runAfter: runAfterHookGallery,
-  },
-  gallery_16x9: {
-    sizes: "(min-width: 45em) 400px, 100vw",
-    classes: ["gallery-16x9"],
-    fallbackWidth: 400,
-    minWidth: 320,
-    maxWidth: 640,
-    steps: 3,
-    runAfter: runAfterHookGallery,
-  },
-  gallery_1x1: {
-    sizes: "(min-width: 45em) 400px, 100vw",
-    classes: ["gallery-1x1"],
-    fallbackWidth: 400,
-    minWidth: 320,
-    maxWidth: 640,
-    steps: 3,
-    runAfter: runAfterHookGallery,
-  },
-  
+	default: {
+		sizes: "(min-width: 65em) 1040px, (min-width: 45em) 800px, 100vw",
+		attributes: {
+			loading: "lazy",
+		},
+		resizedImageUrl: (src, width) => `${src}?nf_resize=fit&w=${width}`,
+		runAfter: runAfterHook,
+	},
+	regular: {
+		classes: ["regular"],
+	},
+	cinemascope: {
+		sizes: "(min-width: 65em) 1280px, (min-width: 45em) 800px, 100vw",
+		classes: ["cinemascope"],
+	},
+	book_thumb: {
+		sizes: "400px, 100vw",
+		classes: ["book-thumb"],
+		figure: "never",
+	},
+	gallery_3x2: {
+		sizes: "(min-width: 45em) 400px, 100vw",
+		classes: ["gallery-3x2"],
+		fallbackWidth: 400,
+		minWidth: 320,
+		maxWidth: 640,
+		steps: 3,
+		runAfter: runAfterHookGallery,
+	},
+	gallery_2x3: {
+		sizes: "(min-width: 45em) 400px, 100vw",
+		classes: ["gallery-2x3"],
+		fallbackWidth: 400,
+		minWidth: 320,
+		maxWidth: 640,
+		steps: 3,
+		runAfter: runAfterHookGallery,
+	},
+	gallery_4x3: {
+		sizes: "(min-width: 45em) 400px, 100vw",
+		classes: ["gallery-4x3"],
+		fallbackWidth: 400,
+		minWidth: 320,
+		maxWidth: 640,
+		steps: 3,
+		runAfter: runAfterHookGallery,
+	},
+	gallery_3x4: {
+		sizes: "(min-width: 45em) 400px, 100vw",
+		classes: ["gallery-3x4"],
+		fallbackWidth: 400,
+		minWidth: 320,
+		maxWidth: 640,
+		steps: 3,
+		runAfter: runAfterHookGallery,
+	},
+	gallery_16x9: {
+		sizes: "(min-width: 45em) 400px, 100vw",
+		classes: ["gallery-16x9"],
+		fallbackWidth: 400,
+		minWidth: 320,
+		maxWidth: 640,
+		steps: 3,
+		runAfter: runAfterHookGallery,
+	},
+	gallery_1x1: {
+		sizes: "(min-width: 45em) 400px, 100vw",
+		classes: ["gallery-1x1"],
+		fallbackWidth: 400,
+		minWidth: 320,
+		maxWidth: 640,
+		steps: 3,
+		runAfter: runAfterHookGallery,
+	},
+	
 };
